28eff06e6912ff3af1d7465a73bbb5dab9b0135d
Tan Pham <tan.phamx@titancorpvn.com>
2024-08-21 11:30:28 +0700

    Page Login

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,7 +1,89 @@
+import React, { useState } from "react";
+import { Button, Divider, Form, Input, message, notification } from "antd";
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
+import { Link, useNavigate } from "react-router-dom";
+import { loginAPI } from "../../services/api";
+
 const LoginPage = () => {
-    return (
-        <>Login Page</>
-    )
-}
+  const [form] = Form.useForm();
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onFinish = async (values) => {
+    setIsLoading(true);
+    const res = await loginAPI(values.username, values.password, 2000);
+    setIsLoading(false);
+
+    if (res.data) {
+      message.success("Đăng nhập thành công");
+      navigate("/");
+    } else {
+      notification.error({
+        message: "Lỗi đăng nhập",
+        description: res.message,
+      });
+    }
+  };
+
+  return (
+    <div style={{ padding: "50px" }}>
+      <h1 style={{ textAlign: "center" }}>ĐĂNG NHẬP</h1>
+      <Divider />
+      <Form
+        form={form}
+        name="basic"
+        layout="vertical"
+        style={{
+          maxWidth: 600,
+          margin: "0 auto",
+        }}
+        onFinish={onFinish}
+      >
+        <Form.Item
+          label="Email"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Please input your username!",
+            },
+          ]}
+        >
+          <Input prefix={<UserOutlined />} placeholder="Email" />
+        </Form.Item>
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+          ]}
+        >
+          <Input
+            prefix={<LockOutlined />}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+        <Form.Item shouldUpdate>
+          {() => (
+            <Button type="primary" htmlType="submit" loading={isLoading}>
+              Đăng nhập
+            </Button>
+          )}
+        </Form.Item>
+        <Divider>Or</Divider>
+        <p className="text text-normal">
+          Chưa có tài khoản ?
+          <span>
+            <Link to="/register"> Đăng ký </Link>
+          </span>
+        </p>
+      </Form>
+    </div>
+  );
+};
 
 export default LoginPage;
--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,24 @@
 import axios from "../utils/axios-customize";
 
 const registerUserAPI = (fullName, email, password, phone) => {
-    const URL_BACKEND = "/api/v1/user/register";
-    const data = {
-        fullName: fullName,
-        email: email,
-        password: password,
-        phone: phone
-    }
-    return axios.post(URL_BACKEND, data)
-}
+  const URL_BACKEND = "/api/v1/user/register";
+  const data = {
+    fullName: fullName,
+    email: email,
+    password: password,
+    phone: phone,
+  };
+  return axios.post(URL_BACKEND, data);
+};
 
-export { registerUserAPI }
+const loginAPI = (email, password, delay) => {
+  const URL_BACKEND = "/api/v1/auth/login";
+  const data = {
+    username: email,
+    password: password,
+    delay: delay,
+  };
+  return axios.post(URL_BACKEND, data);
+};
 
-
+export { registerUserAPI, loginAPI };
